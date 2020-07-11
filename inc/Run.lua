f03a776ee59f266a38da6ea0ab128b8bc38d2fde
TH3BOSS <36484822+TH3BS@users.noreply.github.com>
2020-07-11 04:29:18 +0300

    vتحديث الجديد اصدار 3.0

--- inc/Run.lua
+++ inc/Run.lua
@@ -61,12 +61,13 @@ if not SUDO_USER:match('@[%a%d_]') then
 print('\n\27[1;31m￤ This is Not USERNAME !\n￤هہ‏‏ذآ ليس مـعرف حسـآب تلگرآم , عذرآ آدخل آلمـعرف آلصـحيح آلآن . ')
 create_config(Token)
 end 
-local DirFol = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all')
+local DirFol = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ',''):gsub("\n",'')
 user = {}
 user.username = SUDO_USER
 user.Source  = DirFol
 local url , res = https.request('https://api.th3boss.com/GetID/?Array='..JSON.encode(user))
-if res ~= 200 then
+print('https://api.th3boss.com/GetID/?Array='..JSON.encode(user))
+if (res ~= 200 and res ~= 500) then
 print(res)
 print('\n\27[1;31m￤ Conect is Failed !\n￤ حدث خطـآ في آلآتصـآل بآلسـيرفر , يرجى مـرآسـلهہ‏‏ مـطـور آلسـورس ليتمـگن مـن حل آلمـشـگلهہ‏‏ في آسـرع وقت مـمـگن . !')
 os.exit()
