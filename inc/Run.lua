d1afed066749a46b5ffcd0ff23cf348b70382af8
TH3BOSS <36484822+TH3BS@users.noreply.github.com>
2020-07-11 03:23:48 +0300

    vتحديث الجديد اصدار 3.0

--- inc/Run.lua
+++ inc/Run.lua
@@ -65,7 +65,7 @@ local DirFol = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all')
 user = {}
 user.username = SUDO_USER
 user.Source  = DirFol
-local url , res = https.request('https://api.th3bs.com/GetID/?Array='..JSON.encode(user))
+local url , res = https.request('https://api.th3boss.com/GetID/?Array='..JSON.encode(user))
 if res ~= 200 then
 print(res)
 print('\n\27[1;31m￤ Conect is Failed !\n￤ حدث خطـآ في آلآتصـآل بآلسـيرفر , يرجى مـرآسـلهہ‏‏ مـطـور آلسـورس ليتمـگن مـن حل آلمـشـگلهہ‏‏ في آسـرع وقت مـمـگن . !')
